19341613cb99859ef87360e24669eb53af9f4df4
Darryl Morley <darrylmorley.uk@gmail.com>
2021-01-15 09:27:11 +0000

    Use next/image for home page images

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,8 +1,9 @@
+import Head from 'next/head'
+import Image from 'next/image'
 import CookieConsent from "react-cookie-consent";
+
 import Layout from '../components/Layout'
 import BannerBottom from '../components/BannerBottom'
-// import HomeModal from '../components/Modal'
-import Head from 'next/head'
 
 export default function Home() {
   return (
@@ -10,12 +11,10 @@ export default function Home() {
       <Head>
         <title>FAB Defense (UK)</title>
         <meta name="description" content="Quality Gun Accessories from FAB Defense!" />
-        <meta property="og:title" content="FAB Defense (UK)" />
+        <meta property="og:title" content="FAB Defense" />
         <meta property="og:description" content="Quality Gun Accessories from FAB Defense!" />
         <meta property="og:image" content="/logos/FAB-logo.png" />
       </Head>
-      {/* <Banner /> */}
-      {/* <HomeModal /> */}
       <CookieConsent
         location="bottom"
         buttonText="Okay"
@@ -26,15 +25,44 @@ export default function Home() {
       >
         This website uses cookies for functionality and user experience.{" "}
       </CookieConsent>
-      {/* <div id="app-modal" /> */}
       <main className="hidden lg:mx-60 lg:mt-32 lg:flex lg:justify-center" id="modal">
         <section className="lg:grid lg:grid-cols-3 lg:gap-2">
-          <div><img src="products/FX-AG43B-Black.jpg" alt="FAB Defense AG43B" width="300" /></div>
-          <div><img src="products/FX-REGB-White.jpg" alt="" width="300" /></div>
-          <div><img src="products/FX-GLCOREB-DkGray.jpg" alt="" width="300" /></div>
-          <div><img src="products/FX-TPODG2B-Black.jpg" alt="" width="300" /></div>
-          <div><img src="products/FX-GLCORES-DkGray.jpg" alt="" width="300" /></div>
-          <div><img src="products/FX-USM-Red.jpg" alt="" width="300" /></div>
+          {/* <div><img src="products/FX-AG43B-Black.jpg" alt="FAB Defense AG43B" width="300" /></div> */}
+          <Image
+            src="/products/FX-AG43B-Black.jpg"
+            width={300}
+            height={300}
+            alt="FAB Defense AG43B" />
+          {/* <div><img src="products/FX-REGB-White.jpg" alt="" width="300" /></div> */}
+          <Image
+            src="/products/FX-REGB-White.jpg"
+            width={300}
+            height={300}
+            alt="FAB Defense FX REGB Ergonomic Grip" />
+          {/* <div><img src="products/FX-GLCOREB-DkGray.jpg" alt="" width="300" /></div> */}
+          <Image
+            src="/products/FX-GLCOREB-DkGray.jpg"
+            width={300}
+            height={300}
+            alt="FAB Defense GL Core B Buttstock" />
+          {/* <div><img src="products/FX-TPODG2B-Black.jpg" alt="" width="300" /></div> */}
+          <Image
+            src="/products/FX-TPODG2B-Black.jpg"
+            width={300}
+            height={300}
+            alt="FAB Defense FX TPOD Bipod" />
+          {/* <div><img src="products/FX-GLCORES-DkGray.jpg" alt="" width="300" /></div> */}
+          <Image
+            src="/products/FX-GLCORES-DkGray.jpg"
+            width={300}
+            height={300}
+            alt="FAB Defense GLCORE S Buttstock" />
+          {/* <div><img src="products/FX-USM-Red.jpg" alt="" width="300" /></div> */}
+          <Image
+            src="/products/FX-USM-Red.jpg"
+            width={300}
+            height={300}
+            alt="FAB Defense USM" />
         </section>
       </main>
       <div className="lg:my-40">
