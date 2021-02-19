ac28a4a0351691ca567ee3e9705180b014624bc1
Darryl Morley <darrylmorley.uk@gmail.com>
2021-02-19 22:21:56 +0000

    Add axios rate limit in attempt to resolve build failures

--- pages/api/lightspeed.js
+++ pages/api/lightspeed.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 import refreshToken from './refreshToken'
-
+import rateLimit from 'axios-rate-limit';
 
 async function getHeader() {
   let token = await refreshToken()
@@ -17,37 +17,40 @@ async function getHeader() {
   return axiosConfig
 }
 
+const http = rateLimit(axios.create(), { maxRequests: 1, perMilliseconds: 2000, maxRPS: 1 })
+
 export async function getDelivery() {
   let axiosConfig = await getHeader()
-  return axios.get(`Item.json?itemID=7051&load_relations=["Category", "Images", "ItemShops", "ItemECommerce"]`, axiosConfig).catch(err => console.error(err.data))
+  return http.get(`Item.json?itemID=7051&load_relations=["Category", "Images", "ItemShops", "ItemECommerce"]`, axiosConfig).catch(err => console.error(err.data))
 }
 
 export async function getItems() {
   let axiosConfig = await getHeader()
-  return axios.get(`Item.json?manufacturerID=55&load_relations=["Category", "Images", "ItemShops", "ItemECommerce"]`, axiosConfig).catch(err => console.error(err.data))
+  return http.get(`Item.json?manufacturerID=55&load_relations=["Category", "Images", "ItemShops", "ItemECommerce"]`, axiosConfig).catch(err => console.error(err.data))
 }
 
 export async function getItem(itemID) {
   let axiosConfig = await getHeader()
-  return axios.get(`Item/${itemID}.json?load_relations=["Category", "Images", "ItemShops", "CustomFieldValues", "ItemECommerce"]`, axiosConfig).catch(err => console.error(err.data))
+  return http.get(`Item/${itemID}.json?load_relations=["Category", "Images", "ItemShops", "CustomFieldValues", "ItemECommerce"]`, axiosConfig).catch(err => console.error(err.data))
 }
 
 export async function getMatrixItems() {
   let axiosConfig = await getHeader()
-  return axios.get(`ItemMatrix.json?manufacturerID=55&load_relations=["Category", "Images", "Items", "ItemECommerce"]`, axiosConfig).catch(err => console.error(err.data))
+  return http.get(`ItemMatrix.json?manufacturerID=55&load_relations=["Category", "Images", "Items", "ItemECommerce"]`, axiosConfig)
+  .catch(err => console.error(err.data))
 }
 
 export async function getMatrixItem(itemID) {
   let axiosConfig = await getHeader()
-  return axios.get(`ItemMatrix/${itemID}.json?load_relations=["Category", "Images", "Items", "ItemECommerce"]`, axiosConfig).catch(err => console.error(err.data))
+  return http.get(`ItemMatrix/${itemID}.json?load_relations=["Category", "Images", "Items", "ItemECommerce"]`, axiosConfig).catch(err => console.error(err.data))
 }
 
 export async function createSale(newSale) {
   let axiosConfig = await getHeader()
-  return axios.post(`Sale.json`, newSale, axiosConfig).catch(error => console.error(error.data).then(res => { return res })).catch(err => console.error(err.data))
+  return http.post(`Sale.json`, newSale, axiosConfig).catch(error => console.error(error.data).then(res => { return res })).catch(err => console.error(err.data))
 }
 
 export async function getCategories(categoryID) {
   let axiosConfig = await getHeader()
-  return axios.get(`Category.json?categoryID=IN,${categoryID}&orderby=name`, axiosConfig).catch(err => console.error(err.data))
+  return http.get(`Category.json?categoryID=IN,${categoryID}&orderby=name`, axiosConfig).catch(err => console.error(err.data))
 }
