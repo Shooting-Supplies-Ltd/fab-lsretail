4af9a6ccacb0b056659f91f2a060463db67b2503
Darryl Morley <darrylmorley.uk@gmail.com>
2021-04-12 13:10:16 +0100

    add list on store filter

--- pages/api/lightspeed.js
+++ pages/api/lightspeed.js
@@ -1,10 +1,10 @@
 // @ts-check
-import axios from 'axios'
-import {refreshToken} from './refreshToken'
+import axios from 'axios';
 import rateLimit from 'axios-rate-limit';
+import { refreshToken } from './refreshToken';
 
 async function getHeader() {
-  let token = await refreshToken()
+  const token = await refreshToken();
 
   const header = {
     Authorization: `Bearer ${token}`,
@@ -12,50 +12,73 @@ async function getHeader() {
 
   const axiosConfig = {
     baseURL: `https://api.lightspeedapp.com/API/Account/${process.env.ACCOUNT_ID}/`,
-    headers: header
-  }
+    headers: header,
+  };
 
-  return axiosConfig
+  return axiosConfig;
 }
 
-const http = rateLimit(axios.create(), { maxRequests: 1, perMilliseconds: 2000, maxRPS: 1 })
+const http = rateLimit(axios.create(), { maxRequests: 1, perMilliseconds: 2000, maxRPS: 1 });
 
 export async function getDelivery() {
-  let axiosConfig = await getHeader()
-  return http.get(`Item.json?itemID=7051&load_relations=["Category", "Images", "ItemShops", "ItemECommerce"]`, axiosConfig).catch(err => console.error(err.data))
+  const axiosConfig = await getHeader();
+  return http
+    .get(`Item.json?itemID=7051&load_relations=["Category", "Images", "ItemShops", "ItemECommerce"]`, axiosConfig)
+    .catch((err) => console.error(err.data));
 }
 
 export async function getItems() {
-  let axiosConfig = await getHeader()
-  const items = await http.get(`Item.json?manufacturerID=55&load_relations=["Category", "Images", "ItemShops", "ItemECommerce"]`, axiosConfig).catch(err => console.error(err.data))
-  return items
+  const axiosConfig = await getHeader();
+  const items = await http
+    .get(
+      `Item.json?manufacturerID=55&load_relations=["Category", "Images", "ItemShops", "ItemECommerce"]&ItemECommerce.listOnStore=true`,
+      axiosConfig
+    )
+    .catch((err) => console.error(err.data));
+  return items;
 }
 
 export async function getItem(itemID) {
-  let axiosConfig = await getHeader()
-  const item = await http.get(`Item/${itemID}.json?load_relations=["Category", "Images", "ItemShops", "CustomFieldValues", "ItemECommerce"]`, axiosConfig).catch(err => console.error(err.data))
-  return item
+  const axiosConfig = await getHeader();
+  const item = await http
+    .get(
+      `Item/${itemID}.json?load_relations=["Category", "Images", "ItemShops", "CustomFieldValues", "ItemECommerce"]`,
+      axiosConfig
+    )
+    .catch((err) => console.error(err.data));
+  return item;
 }
 
 export async function getMatrixItems() {
-  let axiosConfig = await getHeader()
-  const matrixItems = await http.get(`ItemMatrix.json?manufacturerID=55&load_relations=["Category", "Images", "Items", "ItemECommerce"]`, axiosConfig)
-  .catch(err => console.error(err.data))
-  return matrixItems
+  const axiosConfig = await getHeader();
+  const matrixItems = await http
+    .get(
+      `ItemMatrix.json?manufacturerID=55&load_relations=["Category", "Images", "Items", "ItemECommerce"]&ItemECommerce.listOnStore=true`,
+      axiosConfig
+    )
+    .catch((err) => console.error(err.data));
+  return matrixItems;
 }
 
 export async function getMatrixItem(itemID) {
-  let axiosConfig = await getHeader()
-  const matrixItem = await http.get(`ItemMatrix/${itemID}.json?load_relations=["Category", "Images", "Items", "ItemECommerce"]`, axiosConfig).catch(err => console.error(err.data))
-  return matrixItem
+  const axiosConfig = await getHeader();
+  const matrixItem = await http
+    .get(`ItemMatrix/${itemID}.json?load_relations=["Category", "Images", "Items", "ItemECommerce"]`, axiosConfig)
+    .catch((err) => console.error(err.data));
+  return matrixItem;
 }
 
 export async function createSale(newSale) {
-  let axiosConfig = await getHeader()
-  return http.post(`Sale.json`, newSale, axiosConfig).catch(error => console.error(error.data).then(res => { return res })).catch(err => console.error(err.data))
+  const axiosConfig = await getHeader();
+  return http
+    .post(`Sale.json`, newSale, axiosConfig)
+    .catch((error) => console.error(error.data).then((res) => res))
+    .catch((err) => console.error(err.data));
 }
 
 export async function getCategories(categoryID) {
-  let axiosConfig = await getHeader()
-  return http.get(`Category.json?categoryID=IN,${categoryID}&orderby=name`, axiosConfig).catch(err => console.error(err.data))
+  const axiosConfig = await getHeader();
+  return http
+    .get(`Category.json?categoryID=IN,${categoryID}&orderby=name`, axiosConfig)
+    .catch((err) => console.error(err.data));
 }
